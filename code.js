fdea68df03278fa67fc220f78b1df6054deb9a61
Gedvydas Kleberis <gedvtdas.kleberis@gmail.com>
2024-12-10 02:09:54 +0200

    added about page css and added reviews to the main page

--- code.js
+++ code.js
@@ -1,20 +1,31 @@
-var express=require("express")
-var bodyParser=require("body-parser")
-var mongoose=require("mongoose")
 
-const app=express()
+let slideIndex = 1;
+showSlides(slideIndex);
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-    extended:true
-}))
-mongoose.connect('')
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-app.get("/",(req,res) =>{
-    res.set({
-        "Allow-access-Allow-Origin" :'*'
-    })
-} ).listen(8080);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-console.log("Listening on port 8080");
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
