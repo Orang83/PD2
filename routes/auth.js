2209bcbc3ea83daa039670cf21169876b8abd7a8
Snoozerbas <aleksandrascupinas@gmail.com>
2024-12-10 13:24:18 +0200

    Add files via upload

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authController = require('../controllers/auth')
+
+const router = express.Router();
+
+
+router.post('/registration', authController.register );
+
+
+
+
+    module.exports = router;
